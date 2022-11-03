e38a27df1c9f6278798531b0b953383223cbb8a0
aditya161216 <59684507+aditya161216@users.noreply.github.com>
2022-11-02 22:57:54 -0400

    Added dummy data

---
+++ src/main/resources/db/migration/V1.0__create_dummy_data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dummy_data
+(
+    id         BIGINT            NOT NULL,
+    name       VARCHAR(100)      NOT NULL,
+    latitude   VARCHAR(100)      NOT NULL,
+    longitude  VARCHAR(100)      NOT NULL,
+
+    PRIMARY KEY (id)
+);
---
+++ src/main/resources/db/migration/V1.0__modify_dummy_data.sql
@@ -0,0 +1,38 @@
+/*INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (1, 'Issec', '92.30.43.21', '34.56.74.21');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (2, 'Snell', '92.33.42.20', '32.56.77.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (3, 'Behrakhris', '90.31.41.10', '29.56.43.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (4, 'Khoury Building', '80.36.40.89', '37.56.40.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (5, 'Ryder Hall', '30.31.41.10', '50.56.43.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (6, 'Shillman Hall', '20.31.41.10', '39.56.43.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (7, 'Kariotis Hall', '20.31.41.13', '39.76.43.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (8, 'Churchill Hall', '90.31.41.10', '31.56.43.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (9, 'Mugar Life Sciences Building', '96.31.41.10', '49.56.43.19');
+
+INSERT INTO dummy_data (id, name, latitude, longitude)
+VALUES (10, 'Curry Student Center', '32.31.41.10', '13.56.46.19');
+
+
+UPDATE dummy_data
+SET name = 'Hayden Center'
+WHERE id = 4;
+
+ */
+
+
