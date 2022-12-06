bc22c6cfb51bc72ac8641312bdc86488a77f4b4e
aditya161216 <59684507+aditya161216@users.noreply.github.com>
2022-12-05 21:11:00 -0500

    Final changes

--- src/main/resources/db/migration/V1.0__create_items_table.sql
+++ src/main/resources/db/migration/V1.0__create_items_table.sql
@@ -6,17 +6,3 @@ CREATE TABLE resources(
     PRIMARY KEY (item_id),
     FOREIGN KEY (location_id) REFERENCES locations(location_id)
 );
-
-
-INSERT INTO resources (item_id, type, location_id)
-VALUES (1, 'Recycling Bin', 1);
-INSERT INTO resources (item_id, type, location_id)
-VALUES (2, 'Wastebasket', 4);
-INSERT INTO resources (item_id, type, location_id)
-VALUES (3, 'Recycling Bin', 2);
-INSERT INTO resources (item_id, type, location_id)
-VALUES (4, 'Compost Bin', 8);
-INSERT INTO resources (item_id, type, location_id)
-VALUES (5, 'Recycling Bin', 5);
-INSERT INTO resources (item_id, type, location_id)
-VALUES (6, 'Wastebasket', 7);
